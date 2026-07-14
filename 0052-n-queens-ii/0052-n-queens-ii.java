40a6b64ffd8beb34739ad614d6697fcc321adb9b
Nishchal Gunda <gundavasharavi@gmail.com>
2026-07-14 10:47:07 +0530

    Time: 1 ms (96.36%), Space: 42 MB (60.53%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int totalNQueens(int n) {
+
+        List<List<String>> r = new ArrayList<>();
+        char[][] board = new char[n][n];
+
+        for (int i = 0; i < n; i++)
+            Arrays.fill(board[i], '.');
+
+        dfs(n,0,new boolean[n],new boolean[2 * n - 1],new boolean[2 * n - 1],board,r);
+
+        return r.size();
+    }
+
+    public void dfs(int n,int i,boolean col[],boolean d1[],boolean d2[],char[][] board, List<List<String>> r) {
+
+        if (i == n) {
+            r.add(construct(board));
+            return;
+        }
+
+        for (int j = 0; j < n; j++) {
+
+            if (col[j] || d1[i + j] || d2[n - 1 + i - j]) {
+                continue;
+            }
+
+            board[i][j] = 'Q';
+            col[j] = d1[i + j] = d2[n - 1 + i - j] = true;
+
+            dfs(n, i + 1, col, d1, d2, board, r);
+
+            board[i][j] = '.';
+            col[j] = d1[i + j] = d2[n - 1 + i - j] = false;
+        }
+    }
+
+    public List<String> construct(char[][] board) {
+        List<String> ls = new ArrayList<>();
+
+        for (int i = 0; i < board.length; i++) {
+            ls.add(String.valueOf(board[i]));
+        }
+
+        return ls;
+    }
+}
+    
